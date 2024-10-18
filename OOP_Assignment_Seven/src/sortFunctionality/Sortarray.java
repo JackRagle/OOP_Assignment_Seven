c6e269818f0d5920e9d716ea7dcdffc3088bc59f
jackd <jackd@Jacks_PC.attlocal.net>
2024-10-18 19:12:02 -0400

    Creation of classes

--- OOP_Assignment_Seven/src/sortFunctionality/Sortarray.java
+++ OOP_Assignment_Seven/src/sortFunctionality/Sortarray.java
@@ -1,5 +1,66 @@
 package sortFunctionality;
 
-public class Sortarray {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Sortarray {
+	
+	public static int[] array;
+	
+	public static int[] createRandomArray(int arrayLength) {
+		
+		array = new int[arrayLength];
+		for(int i = 0; i < arrayLength; i++) {
+			array[i] = (int) (Math.random() * 101);
+		}
+		
+		return array;
+		
+	}
+	
+	public static void writeArrayToFile(int[] array, String filename) throws IOException {
+		
+		Sortarray.array = array;
+			
+		FileWriter output = new FileWriter(filename);
+		
+		for(int i = 0; i < array.length; i++) {
+			
+			output.write(array[i]);
+			output.write("\n");
+			
+		}
+		
+		output.close();
+		
+	}
+	
+	public static int[] readFileToArray(String filename) throws IOException {
+		
+		BufferedReader input = new BufferedReader(new FileReader(filename));
+		
+		int numLines = 0;
+		
+		while(input.readLine() != null) {
+			numLines++;
+		}
+		
+		array = new int[numLines];
+		
+		for(int i = 0; i < numLines; i++) {
+			array[i] = input.read();
+		}
+		
+		input.close();
+		
+		return array;
+		
+	}
+	
+	public static void bubbleSort(int[] array) {
+		
+	}
+	
 }
--- OOP_Assignment_Seven/src/sortInAction/TestArray.java
+++ OOP_Assignment_Seven/src/sortInAction/TestArray.java
@@ -1,10 +1,23 @@
 package sortInAction;
 
+import java.io.File;
+import java.io.IOException;
+
+import sortFunctionality.Sortarray;
+
 public class TestArray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		File inputFile = new File(args[0]);
+		
+		int[] testArray = Sortarray.createRandomArray(5);
+		for(int i = 0; i < 5; i++) {
+			System.out.println(testArray[i]);
+		}
+		
+		
+		
 	}
 
 }
