562125014b1ce38cd9cff591ffd4cc086a96c12f
jackd <jackd@Jacks_PC.attlocal.net>
2024-10-18 18:22:06 -0400

    Creation of classes

---
+++ OOP_Assignment_Seven/src/sortFunctionality/Sortarray.java
@@ -0,0 +1,5 @@
+package sortFunctionality;
+
+public class Sortarray {
+
+}
---
+++ OOP_Assignment_Seven/src/sortInAction/TestArray.java
@@ -0,0 +1,10 @@
+package sortInAction;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
